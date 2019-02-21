854053151608a3106241fbb14a7a9ae28ef99be8
y9luiz <y9luiz@hotmail.com>
2019-02-21 10:54:02 -0300

    updated to work witg the tracker base class

--- tracking/klt_tracker.h
+++ tracking/klt_tracker.h
@@ -1,7 +1,7 @@
 /* 
  *  Software License Agreement (BSD License)
  *
- *  Copyright (c) 2016, Natalnet Laboratory for Perceptual Robotics
+ *  Copyright (c) 2016-2018, Natalnet Laboratory for Perceptual Robotics
  *  All rights reserved.
  *  Redistribution and use in source and binary forms, with or without modification, are permitted provided
  *  that the following conditions are met:
@@ -31,6 +31,7 @@
 #include <fstream>
 #include <opencv2/core/core.hpp>
 
+#include <feature_tracker.h>
 #include <common_types.h>
 
 /*
@@ -41,114 +42,34 @@
  * Author: Bruno Marques F. da Silva
  * brunomfs@gmail.com
  */
-class KLTTracker
+class KLTTracker : public FeatureTracker
 {
 
 protected:
 
-	//Flag used to inform if the tracker was initialized
-	bool initialized_;
-
-	//Current frame number
-	int frame_idx_;
-
-	//Previous greyscale frame
-	cv::Mat prev_frame_gray_;
-
-	//Current greyscale frame
-	cv::Mat curr_frame_gray_;
-
-	//Minimum allowed number of tracked points
-	int min_pts_;
-
-	//Maximum allowed number of tracked points
-	int max_pts_;
-
-	//(REMOVE) Count of inliers in the current frame
-	int num_inliers_;
-
-	//Time spent processing each operation (detection, tracking, motion estimation, etc.)
-	std::vector<float> op_time_;
-
-	//Output file with tracking information
-	std::ofstream tracking_info_;
-
-	//Output file with timing information
-	std::ofstream timing_info_;
-
-	//Output file with keypoint distribution information
-	std::ofstream heatmap_info_;
-
 	//Detects keypoints in the current frame
-	virtual void detect_keypoints();
+	void detect_keypoints();
 
 	//Adds keypoints detected in the previous frame to the tracker
-	virtual void add_keypoints();
+	void add_keypoints();
 
 	//Updates internal buffers (updates the previous points (in the current frame)
 	//with the current points (from the previous frame))
-	virtual void update_buffers();
-
-	//Returns the average track length
-	virtual float get_avg_track_length();
-
-	//Prints all data associated with the tracked keypoints
-	virtual void print_track_info();
-
-	//Sum the time spent in each operation and return the total time computing a single frame
-	float get_time_per_frame();
-
-	//Write tracking information (n. of points, inliers, etc.) to file
-	virtual bool write_tracking_info();
-
-	//Write timing information (ms spent in each operation) to file
-	virtual bool write_timing_info();
-
-	//Write all current keypoints to file
-	virtual bool write_heatmap_info();
+	void update_buffers();
 
 public:
-
-	//Tracklets: history of each point as a vector of point2f
-	std::vector<Tracklet> tracklets_;
-
-	//Previous positions of the tracked points
-	std::vector<cv::Point2f> prev_pts_;
-
-	//Current positions of the tracked points
-	std::vector<cv::Point2f> curr_pts_;
-
-	//Points added in the previous frame
-	std::vector<cv::Point2f> added_pts_;
-
-	//Tells if a point is inlier or not
-	std::vector<int> is_inlier_;
 
 	//Default constructor
 	KLTTracker();
 
-	//Constructor with the minimum and maximum number of tracked points
-	KLTTracker(int min_pts, int max_pts);
-
-	virtual ~KLTTracker()
-	{
-		tracking_info_.close();
-		timing_info_.close();
-		heatmap_info_.close();
-	}
+	//Constructor with the minimum number of tracked points, maximum number of tracked points and flag to log statistics
+	KLTTracker(const int min_pts, const int max_pts, const bool log_stats = false);
 
 	/*
 	 * Main member function: tracks keypoints between the current frame and the previous.
 	 * Returns true if the current frame is a keyframe.
 	 */
-	virtual bool track(cv::Mat img);
-
-	/* Sets the output files for tracking and timing information.
-	 * If not called, the system will not write any information to file.
-	 */
-	void initialize_logger(const std::string timing_file_name,
-		                   const std::string tracking_file_name,
-			               const std::string heatmap_file_name);
+	bool track(cv::Mat img);
 };
 
 #endif /* INCLUDE_KLT_TRACKER_H_ */
