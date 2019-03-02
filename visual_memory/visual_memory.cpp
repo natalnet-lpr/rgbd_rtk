d4f72c62bb5fe68236713662f4f266413d5a48b5
y9luiz <y9luiz@hotmail.com>
2019-03-02 14:40:08 -0300

    these files are the main on class, here is implemented the visual memory based on feature matches

---
+++ visual_memory/visual_memory.cpp
@@ -0,0 +1,136 @@
+#include <visual_memory.h>
+#include <iostream>
+
+VisualMemory::VisualMemory(){
+    matcher_ = new cv::FlannBasedMatcher();
+
+}
+VisualMemory::VisualMemory(std::vector<cv::Mat> trainDescriptorsSet){
+    trainDescriptorsSet_ = trainDescriptorsSet;
+    matcher_ = new cv::FlannBasedMatcher();
+
+}
+VisualMemory::VisualMemory(cv::Ptr<cv::DescriptorMatcher> matcher){
+    this->matcher_ = matcher;
+}
+void VisualMemory::create(std::vector<cv::Mat> trainDescriptorsSet){
+    trainDescriptorsSet_ = trainDescriptorsSet;
+
+
+}
+
+
+ void VisualMemory::add(cv::Mat trainDescriptors){
+    //trainDescriptorsSet_.push_back(trainDescriptors);
+    matcher_->add(trainDescriptors);
+ }
+
+
+bool VisualMemory::sort_assistant(std::pair<int,int> a,std::pair<int,int>  b){
+    return (a.first>b.first);
+
+}
+
+std::vector<int> VisualMemory::searchDescriptor(cv::Mat queryDescriptor,int n){
+    std::vector< std::vector<cv::DMatch>> matches; //isso é para o knnmatches       
+    //std::vector<DMatch> matches;  
+
+    if(!matcher_->getTrainDescriptors().empty())
+    {
+        matcher_->train();
+        matcher_->knnMatch(queryDescriptor,matches,2); //usando o knnmatches
+        //matcher_->match(queryDescriptor,matches);
+
+
+    }
+    else {
+        matcher_->knnMatch(queryDescriptor,trainDescriptorsSet_,matches,2); //usando o knnmatches
+        //matcher_->match(queryDescriptor,trainDescriptors_,matches);
+
+
+    }
+
+    //the first one is the amount of matches
+    //the second is the imgidx
+    
+    std::vector< std::pair<int,int> > cont(matches.size());
+    
+    for(int i=0;i<matches.size();i++){// isso é para o knn matches
+
+        for(int j=0;j<matches[i].size();j++){
+            int idx = matches[i][j].imgIdx;
+           
+            cont[idx].first ++;
+            
+
+             cont[idx].second = idx;
+
+            
+        }
+
+    }
+
+
+   /* for(int j=0;j<matches.size();j++){
+            int idx = matches[j].imgIdx;
+           
+            cont[idx].first ++;
+            
+
+             cont[idx].second = idx;
+
+            
+    }
+    */
+    
+    std::sort(cont.begin(),cont.end(),sort_assistant);
+
+    std::vector<int> output;
+
+    for(int i=0;i<n;i++){
+        std::cout<<"índice da imagem "<< cont[i].second<<" quantidade de matchings "<<cont[i].first<<std::endl;
+        output.push_back(cont[i].second);
+
+    }  
+
+    return output;
+
+
+}
+
+std::vector<int> VisualMemory::searchDescriptor(cv::Mat queryDescriptor, int n, cv::Ptr<cv::DescriptorMatcher> matcher){
+    
+    std::vector< std::vector<cv::DMatch>> matches; //isso é para o knnmatches       
+    matcher->train();
+    matcher->knnMatch(queryDescriptor,matches,3); //usando o knnmatches
+   
+    std::vector< std::pair<int,int> > cont(matches.size());
+    
+    for(int i=0;i<matches.size();i++){// isso é para o knn matches
+
+        for(int j=0;j<matches[i].size();j++){
+            int idx = matches[i][j].imgIdx;
+           
+            cont[idx].first ++;
+            
+
+             cont[idx].second = idx;
+
+            
+        }
+
+    }
+    
+    std::sort(cont.begin(),cont.end(),sort_assistant);
+
+    std::vector<int> output;
+
+    for(int i=0;i<n;i++){
+        std::cout<<"índice da imagem "<< cont[i].second<<" quantidade de matchings "<<cont[i].first<<std::endl;
+        output.push_back(cont[i].second);
+
+    }
+    return output;
+
+
+}
---
+++ visual_memory/visual_memory.h
@@ -0,0 +1,40 @@
+#include "opencv2/features2d.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include <vector>
+#include <utility> //std::pair
+
+using namespace cv::xfeatures2d;
+/*
+ * Author: Luiz Felipe Maciel Correia
+ * y9luizufrn@gmail.com
+ */
+
+class VisualMemory{
+   protected:
+        
+        std::vector<cv::Mat> trainDescriptorsSet_;
+
+        cv::Ptr<cv::DescriptorMatcher> matcher_;
+       static bool sort_assistant(std::pair<int,int> a,std::pair<int,int>  b);
+    public:
+      //Default constructor
+        VisualMemory();
+      //Constructor if you desire dont store your trainDescriptorSet in  cv::Ptr<cv::DescriptorMatcher> matcher_;
+        VisualMemory(std::vector<cv::Mat> trainDescriptorsSet);
+      // Constructor if you wnat to store your descriptors in   cv::Ptr<cv::DescriptorMatcher> matcher_; (recommended)
+        VisualMemory(cv::Ptr<cv::DescriptorMatcher> matcher);
+      //create a VisualMemory storing the descriptors in         std::vector<cv::Mat> trainDescriptorsSet_; (not recommended)
+       void create(std::vector<cv::Mat> trainDescriptorsSet_);
+      //add descriptors in   cv::Ptr<cv::DescriptorMatcher> matcher_;
+       void add(cv::Mat trainDescriptors);
+      //search a descriptor of a query image in one of two structures  std::vector<cv::Mat> trainDescriptorsSet_ or         cv::Ptr<cv::DescriptorMatcher> matcher_;
+
+       std::vector<int> searchDescriptor(cv::Mat queryDescriptor,int n);
+        
+       std::vector<int> searchDescriptor(cv::Mat, int n, cv::Ptr<cv::DescriptorMatcher> matcher);
+
+
+
+
+};
