b29118591625da11513e10a5f8e7b832fd463754
Bruno Silva <brunomfs@gmail.com>
2019-12-12 17:01:30 -0300

    Cosmetics in ConfigLoader class.

--- io/config_loader.h
+++ io/config_loader.h
@@ -1,3 +1,33 @@
+/* 
+ *  Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2016-2019, Natalnet Laboratory for Perceptual Robotics
+ *  All rights reserved.
+ *  Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ *  that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice, this list of conditions and
+ *     the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *     the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 
+ *  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
+ *     promote products derived from this software without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Authors:
+ *
+ *  Rodrigo Sarmento Xavier
+ *  Bruno Silva
+ */
+
 #ifndef INCLUDE_CONFIG_LOADER_H_
 #define INCLUDE_CONFIG_LOADER_H_
 
@@ -7,15 +37,25 @@
 
 using namespace std;
 
-class ConfigLoader{
+class ConfigLoader
+{
     public:
+        //.yml having the camera calibration intrinsics
         string camera_calibration_file_;
+
+        //full path to the index with RGB/depth to be processed
         string index_file_;
+
+        //Used in marker detection: ARUCO dictionary of expected markers in the scene
         string aruco_dic_;
-        double aruco_distance_;
+
+        //Used in marker detection: maximum distance for a marker to be valid
+        double aruco_max_distance_;
+
+        //Used in marker detection: size of expected markers in the scene
         float aruco_marker_size_;
         
-    void loadParams(string filename);
+    void loadParams(const string& filename);
 };
 
 #endif
