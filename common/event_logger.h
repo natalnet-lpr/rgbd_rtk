8d17a0da395e1b1d595809080638b4ac53ecffec
Bruno Silva <brunomfs@gmail.com>
2019-12-13 17:03:47 -0300

    Initial implementation.

---
+++ common/event_logger.h
@@ -0,0 +1,75 @@
+/* 
+ *  Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2016-2019, Natalnet Laboratory for Perceptual Robotics
+ *  All rights reserved.
+ *  Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ *  that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice, this list of conditions and
+ *     the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *     the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 
+ *  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
+ *     promote products derived from this software without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef INCLUDE_EVENT_LOGGER_H_
+#define INCLUDE_EVENT_LOGGER_H_
+
+#include <cstdio>
+
+class EventLogger
+{
+
+private:
+
+	//Output file for the log file
+	FILE* log_file_;
+
+public:
+
+	/**
+	 * Default constructor
+	 */
+	EventLogger()
+	{
+		log_file_ = fopen("log.txt", "w");
+	}
+
+	/**
+	 * Constructor specifying the log file name
+	 * @param file_name: name of the log file
+	 */
+	EventLogger(const char* file_name)
+	{
+		log_file_ = fopen(file_name, "w");
+	}
+
+	~EventLogger()
+	{
+		fclose(log_file_);
+	}
+
+	void print(const char *format, ...);
+
+	void printDebug();
+
+	void printInfo();
+
+	void printWarning();
+
+	void printError();
+};
+
+#endif /* INCLUDE_EVENT_LOGGER_H_ */
