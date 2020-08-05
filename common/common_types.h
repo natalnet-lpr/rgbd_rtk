71ecc8ddc0b60f39cccf772845ce0d5e5b9ebab7
RodrigoSarmento <rodrigosarmentoxx@gmail.com>
2020-08-05 16:45:21 -0300

    new functions added in reconstruction visualizer related to Edge and KeyFrames. Edge structure added in common_types as well

--- common/common_types.h
+++ common/common_types.h
@@ -30,11 +30,11 @@
 #ifndef INCLUDE_COMMON_TYPES_H_
 #define INCLUDE_COMMON_TYPES_H_
 
-#include <vector>
 #include <Eigen/Geometry>
 #include <opencv2/core/core.hpp>
-#include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <vector>
 
 typedef pcl::PointXYZRGB PointT;
 
@@ -45,10 +45,10 @@ typedef pcl::PointXYZRGB PointT;
  */
 struct Tracklet
 {
-	/* Index of the frame in which the keypoint first appeared */
-	int start_;
+    /* Index of the frame in which the keypoint first appeared */
+    int start_;
 
-	/* History of image projections of the keypoints along consecutive
+    /* History of image projections of the keypoints along consecutive
 	 * frames.
 	 * m_pts2D[i] holds the position of the point at index start_ + i,
 	 * that is,
@@ -56,28 +56,31 @@ struct Tracklet
 	 * m_pts2D[1] holds the position of the point at index start_ + 1
 	 * and so on.
 	 */
-	std::vector<cv::Point2f> pts2D_;
+    std::vector<cv::Point2f> pts2D_;
 
-	/*
+    /*
 	 * At position i, the vector stores the index of the 3D point
 	 * at the 3D point cloud start_ + i.
 	 */
-	std::vector<int> cloud_indices_;
+    std::vector<int> cloud_indices_;
 
-	/*
+    /*
 	 * At position i, the vector stores the index of the keypoint
 	 * corresponding to the 2D point stored in pts2D_.
 	 */
-	std::vector<int> keypoint_indices_;
+    std::vector<int> keypoint_indices_;
 
-	/* Initializes a tracklet with start_ = 0 */
-	Tracklet() : start_(0) {}
+    /* Initializes a tracklet with start_ = 0 */
+    Tracklet()
+        : start_(0)
+    {
+    }
 
-	/* Initializes a tracklet with start_ = idx */
-	Tracklet(const int idx)
-	{
-		start_ = idx;
-	}
+    /* Initializes a tracklet with start_ = idx */
+    Tracklet(const int idx)
+    {
+        start_ = idx;
+    }
 };
 
 /*
@@ -86,124 +89,146 @@ struct Tracklet
  */
 struct Intrinsics
 {
-	/* Focal distance in the horizontal direction */
-	double fx_;
+    /* Focal distance in the horizontal direction */
+    double fx_;
 
-	/* Focal distance in the vertical direction */
-	double fy_;
+    /* Focal distance in the vertical direction */
+    double fy_;
 
-	/* Projection center coordinate in the horizontal direction */
-	double cx_;
+    /* Projection center coordinate in the horizontal direction */
+    double cx_;
 
-	/* Projection center coordinate in the vertical direction */
-	double cy_;
+    /* Projection center coordinate in the vertical direction */
+    double cy_;
 
-	/* Scale factor for the Kinect depth */
-	double scale_;
+    /* Scale factor for the Kinect depth */
+    double scale_;
 
-	/* Distance between cameras (used in stereo) */
-	double baseline_;
+    /* Distance between cameras (used in stereo) */
+    double baseline_;
 
-	/* First radial distortion coefficient (d0) */
-	double k1_;
+    /* First radial distortion coefficient (d0) */
+    double k1_;
 
-	/* Second radial distortion coefficient (d1) */
-	double k2_;
+    /* Second radial distortion coefficient (d1) */
+    double k2_;
 
-	/* First tangential distortion coefficient (d2) */
-	double p1_;
+    /* First tangential distortion coefficient (d2) */
+    double p1_;
 
-	/* Second tangential distortion coefficient (d3) */
-	double p2_;
+    /* Second tangential distortion coefficient (d3) */
+    double p2_;
 
-	/* Third radial distortion coefficient (d4) */
-	double k3_;
+    /* Third radial distortion coefficient (d4) */
+    double k3_;
 
-	/* Default constructor: zero for all parameters */
-	Intrinsics(): fx_(0.0), fy_(0.0), cx_(0.0), cy_(0.0),
-			      scale_(0.0), baseline_(0.0), k1_(0.0), k2_(0.0), p1_(0.0),
-			      p2_(0.0), k3_(0.0){}
+    /* Default constructor: zero for all parameters */
+    Intrinsics()
+        : fx_(0.0)
+        , fy_(0.0)
+        , cx_(0.0)
+        , cy_(0.0)
+        , scale_(0.0)
+        , baseline_(0.0)
+        , k1_(0.0)
+        , k2_(0.0)
+        , p1_(0.0)
+        , p2_(0.0)
+        , k3_(0.0)
+    {
+    }
 
-	/*
+    /*
 	 * Constructor for the Freiburg1, Freiburg2, Freiburg3 TUM datasets
 	 * or for the Kinect factor defaults.
 	 */
-	Intrinsics(const int& set)
-	{
-		switch(set)
-		{
-			case 1:
-				fx_ = 517.3;
-				fy_ = 516.5;
-				cx_ = 318.6;
-				cy_ = 255.3;
-				scale_ = 5000;
-				baseline_ = 0.0; //not used by Kinect
-				//k1_ = 0.2624;
-				//k2_ = -0.9531;
-				//p1_ = -0.0054;
-				//p2_ = 0.0026;
-				//k3_ = 1.1633;
-				k1_ = 0.0;
-				k2_ = 0.0;
-				p1_ = 0.0;
-				p2_ = 0.0;
-				k3_ = 0.0;
-				break;
-			case 2:
-				fx_ = 520.9;
-				fy_ = 521.0;
-				cx_ = 325.1;
-				cy_ = 249.7;
-				scale_ = 5000;
-				baseline_ = 0.0; //not used by Kinect
-				//k1_ = 0.2312;
-				//k2_ = -0.7849;
-				//p1_ = -0.0033;
-				//p2_ = -0.0001;
-				//k3_ = 0.9172;
-				k1_ = 0.0;
-				k2_ = 0.0;
-				p1_ = 0.0;
-				p2_ = 0.0;
-				k3_ = 0.0;
-				break;
-			case 3:
-				fx_ = 535.4;
-				fy_ = 539.2;
-				cx_ = 320.1;
-				cy_ = 247.6;
-				scale_ = 5000;
-				baseline_ = 0.0; //not used by Kinect
-				k1_ = 0.0;
-				k2_ = 0.0;
-				p1_ = 0.0;
-				p2_ = 0.0;
-				k3_ = 0.0;
-				break;
-			default:
-				fx_ = 525.0;
-				fy_ = 525.0;
-				cx_ = 319.5;
-				cy_ = 239.5;
-				scale_ = 5000;
-				baseline_ = 0.0; //not used by Kinect
-				k1_ = 0.0;
-				k2_ = 0.0;
-				p1_ = 0.0;
-				p2_ = 0.0;
-				k3_ = 0.0;
-				break;
-		}
-	}
-	/*
+    Intrinsics(const int& set)
+    {
+        switch (set)
+        {
+        case 1:
+            fx_ = 517.3;
+            fy_ = 516.5;
+            cx_ = 318.6;
+            cy_ = 255.3;
+            scale_ = 5000;
+            baseline_ = 0.0; //not used by Kinect
+            //k1_ = 0.2624;
+            //k2_ = -0.9531;
+            //p1_ = -0.0054;
+            //p2_ = 0.0026;
+            //k3_ = 1.1633;
+            k1_ = 0.0;
+            k2_ = 0.0;
+            p1_ = 0.0;
+            p2_ = 0.0;
+            k3_ = 0.0;
+            break;
+        case 2:
+            fx_ = 520.9;
+            fy_ = 521.0;
+            cx_ = 325.1;
+            cy_ = 249.7;
+            scale_ = 5000;
+            baseline_ = 0.0; //not used by Kinect
+            //k1_ = 0.2312;
+            //k2_ = -0.7849;
+            //p1_ = -0.0033;
+            //p2_ = -0.0001;
+            //k3_ = 0.9172;
+            k1_ = 0.0;
+            k2_ = 0.0;
+            p1_ = 0.0;
+            p2_ = 0.0;
+            k3_ = 0.0;
+            break;
+        case 3:
+            fx_ = 535.4;
+            fy_ = 539.2;
+            cx_ = 320.1;
+            cy_ = 247.6;
+            scale_ = 5000;
+            baseline_ = 0.0; //not used by Kinect
+            k1_ = 0.0;
+            k2_ = 0.0;
+            p1_ = 0.0;
+            p2_ = 0.0;
+            k3_ = 0.0;
+            break;
+        default:
+            fx_ = 525.0;
+            fy_ = 525.0;
+            cx_ = 319.5;
+            cy_ = 239.5;
+            scale_ = 5000;
+            baseline_ = 0.0; //not used by Kinect
+            k1_ = 0.0;
+            k2_ = 0.0;
+            p1_ = 0.0;
+            p2_ = 0.0;
+            k3_ = 0.0;
+            break;
+        }
+    }
+    /*
 	 * Constructor for a perspective camera with given focal lengths
 	 * and center of projection (scale is 1.0).
 	 */
-	Intrinsics(const double& fx, const double& fy, const double& cx, const double& cy,
-		       const double& baseline): fx_(fx), fy_(fy), cx_(cx), cy_(cy), scale_(1.0),
-	           baseline_(baseline), k1_(0.0), k2_(0.0), p1_(0.0), p2_(0.0), k3_(0.0)
-	{}
+    Intrinsics(const double& fx, const double& fy, const double& cx, const double& cy,
+        const double& baseline)
+        : fx_(fx)
+        , fy_(fy)
+        , cx_(cx)
+        , cy_(cy)
+        , scale_(1.0)
+        , baseline_(baseline)
+        , k1_(0.0)
+        , k2_(0.0)
+        , p1_(0.0)
+        , p2_(0.0)
+        , k3_(0.0)
+    {
+    }
 };
 
 /*
@@ -221,17 +246,65 @@ struct Keyframe
     //RGB image of the keyframe
     cv::Mat img_;
 
+    //Keyframe name
+    std::string name_;
+
     //RGB-D point cloud of the keyframe (in local reference frame)
     pcl::PointCloud<PointT>::Ptr local_cloud_;
 
     //Keypoints detected in the keyframe
     std::vector<cv::Point2f> keypoints_;
 
-    Keyframe() : 
-        idx_(0),
-        pose_(Eigen::Affine3f::Identity()),
-        local_cloud_(new pcl::PointCloud<PointT>)
-    {}
+    Keyframe()
+        : idx_(0)
+        , pose_(Eigen::Affine3f::Identity())
+        , local_cloud_(new pcl::PointCloud<PointT>)
+    {
+    }
 };
 
+/**
+ * Edge saves two vertix poses that makes an edge
+ * it also saves the id of those poses and the name of the edge
+ */
+struct Edge
+{
+    //Id of vertex "from"
+    int id_from_;
+
+    //Id of vertex "to"
+    int id_to_;
+
+    //Position of vertex "from"
+    Eigen::Vector3d pose_from_;
+
+    //Position of vertex "to
+    Eigen::Vector3d pose_to_;
+
+    //Edge name
+    std::string name_;
+
+    Edge()
+        : id_from_(-1)
+        , id_to_(-1)
+    {
+    }
+
+    /**
+	 * Edge Constructor
+	 * @param from_id int id of vertex from
+	 * @param to_id int id of vertex to
+	 * @param from pose of vertex from
+	 * @param to pose of vertex to
+	 * @param name Edge name(optional)
+	 */
+    Edge(const int& from_id, const int& to_id, const Eigen::Vector3d& from, const Eigen::Vector3d& to, const std::string& name = "")
+    {
+        id_from_ = from_id;
+        id_to_ = to_id;
+        pose_from_ = from;
+        pose_to_ = to;
+        name_ = name;
+    }
+};
 #endif /* COMMON_TYPES_H_ */
--- visualization/reconstruction_visualizer.h
+++ visualization/reconstruction_visualizer.h
@@ -27,106 +27,175 @@
 #ifndef INCLUDE_RECONSTRUCTION_VISUALIZER_H_
 #define INCLUDE_RECONSTRUCTION_VISUALIZER_H_
 
-#include <cstring>
 #include <Eigen/Geometry>
-#include <pcl/visualization/pcl_visualizer.h>
+#include <cstring>
 #include <pcl/common/boost.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
 #include <common_types.h>
+#include <event_logger.h>
 
 typedef boost::shared_ptr<pcl::visualization::PCLVisualizer> PCLVisualizerPtr;
 
 class ReconstructionVisualizer
 {
 private:
-	//Number of point clouds added to the visualizer
-	int num_clouds_;
+    //Number of point clouds added to the visualizer
+    int num_clouds_;
 
-	//Number of lines added to the visualizer
-	int num_lines_;
+    //Number of lines added to the visualizer
+    int num_lines_;
 
-	//Number of reference frames added to the visualizer
-	int num_ref_frames_;
+    //Number of reference frames added to the visualizer
+    int num_ref_frames_;
 
-	//PointCloudsLibrary 3D visualizer (only works with a pointer to the object)
-	PCLVisualizerPtr viewer_;
+    //Number of keyframes added to the visualizer
+    int num_keyframes_;
 
-	//Previous position of the camera
-	pcl::PointXYZ prev_pos_;
+    //Number of edged added to the visualizer
+    int num_edges_;
 
-	//Current position of the camera
-	pcl::PointXYZ curr_pos_;
+    //PointCloudsLibrary 3D visualizer (only works with a pointer to the object)
+    PCLVisualizerPtr viewer_;
+
+    //Previous position of the camera
+    pcl::PointXYZ prev_pos_;
+
+    //Current position of the camera
+    pcl::PointXYZ curr_pos_;
 
 public:
-	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-	/**
+    /**
 	 * Default constructor
 	 */
-  ReconstructionVisualizer();
+    ReconstructionVisualizer();
 
-
-	/**
+    /**
 	 * Constructor with the window title as a string
-	 * @param string 
+	 * @param title Title as String
 	 */
-  ReconstructionVisualizer(const std::string& title);
+    ReconstructionVisualizer(const std::string& title);
 
-	/**
+    /**
+	   * Adds a line between the previous and the current camera position
+	   * @param pose current camera pose 
+	   */
+    void addCameraPath(const Eigen::Affine3f& pose);
+
+    /** Adds an edge from the graph to the PCLVisualizer as an arrow.
+     * @param edge as Edge(common_types.h)
+     * @param color of the arrow(blue as default) 
+     */
+    void addEdge(const Edge& edge, const Eigen::Vector3f& color = Eigen::Vector3f(0.0, 0.0, 1.0));
+
+    /**
+     * Adds all given edges to the PCLVisualizer.
+     * @param edges as a vector of Edge(common_types.h)
+     * @param color of the arrow(blue as default) 
+     */
+    void addEdges(const std::vector<Edge>& edges, const Eigen::Vector3f& color = Eigen::Vector3f(0.0, 0.0, 1.0));
+
+    /**
+     * Adds the optimized version of the given edges to the PCLVisualizer.
+     * @param edges as a vector of Edge(common_types.h)
+     * @param color of the arrow(blue as default) 
+     */
+    void addOptimizedEdges(const std::vector<Edge>& edges, const Eigen::Vector3f& color = Eigen::Vector3f(0.0, 0.0, 1.0));
+
+    /**
 	 * Adds a ref. frame with the given pose to the 3D reconstruction
-	 * @param ref pose and string 
+	 * @param pose Pose to be added
+     * @param text text of the pose in visualization
 	 */
-  void addReferenceFrame(const Eigen::Affine3f& pose, const std::string& text);
+    void addReferenceFrame(const Eigen::Affine3f& pose, const std::string& text);
 
-	/**
+    /**
+	 * Adds a keyFrame with the given pose to the 3D reconstruction
+     * @param kf Keyframe to be added
+     * @param text text of the pose in visualization 
+	 */
+    void addKeyframe(const Keyframe kf, const std::string& text);
+
+    /**
 	 * Adds a point cloud with the given pose to the 3D reconstruction
-	 * @param point cloud and pose
+	 * @param cloud PointCloud to be added
+     * @param pose pose of the 3d Reconstruction 
 	 */
-  void addPointCloud(const pcl::PointCloud<PointT>::Ptr& cloud, const Eigen::Affine3f& pose);
+    void addPointCloud(const pcl::PointCloud<PointT>::Ptr& cloud, const Eigen::Affine3f& pose);
 
-
-	/**
-	 * Adds a point cloud after quantization (uniform sampling) with the given pose to the 3D reconstruction
-	 * @param point cloud, radius and pose
+    /**
+	 * Receives a point cloud and quantize it (uniform sampling) with the given pose to the 3D reconstruction
+	 * @param cloud point cloud after quantization 
+	 * @param radius radius search of uniform sampling  
+     * @param pose of the point cloud 
 	 */
-  void addQuantizedPointCloud(const pcl::PointCloud<PointT>::Ptr& cloud, const float& radius, const Eigen::Affine3f& pose);
+    void addQuantizedPointCloud(const pcl::PointCloud<PointT>::Ptr& cloud, const float& radius, const Eigen::Affine3f& pose);
 
-	/**
+    /**
+     * Removes an edge from the PCLVisualizer.
+     * @param edge to be removed as a Edge(common_types.h)
+     */
+    void removeEdge(const Edge& edge);
+
+    /** 
+     * Removes all given edges from the PCLVisualizer.
+     * @param edges to be removed as a Vector of Edge(common_types.h)
+    */
+    void removeEdges(const std::vector<Edge>& edges);
+
+    /**
 	 * Views a ref. frame with the given pose in the 3D reconstruction
-	 * @param pose and text("cam" as default)
+	 * @param pose pose of the frame
+     * @param text pose text in visualization ("cam" as default)
 	 */
-  void viewReferenceFrame(const Eigen::Affine3f& pose, const std::string& text="cam");
+    void viewReferenceFrame(const Eigen::Affine3f& pose, const std::string& text = "cam");
 
-	/**
+    /**
 	 * Views a point cloud in the 3D reconstruction
-	 * @param point cloud and pose
+	 * @param cloud PointCloud 
+     * @param pose pose of the 3d Reconstruction 
 	 */
-  void viewPointCloud(const pcl::PointCloud<PointT>::Ptr& cloud, const Eigen::Affine3f& pose);
+    void viewPointCloud(const pcl::PointCloud<PointT>::Ptr& cloud, const Eigen::Affine3f& pose);
 
-	/**
-	 * Views a point cloud after quantization (uniform sampling) in the 3D reconstruction
-	 * @param point cloud, radius and pose
+    /**
+	 * Views a point cloud after quantization (uniform sampling) with the given pose to the 3D reconstruction
+	 * @param cloud point cloud after quantization 
+	 * @param radius radius search of uniform sampling  
+     * @param pose of the point cloud 
 	 */
-  void viewQuantizedPointCloud(const pcl::PointCloud<PointT>::Ptr& cloud, const float& radius, const Eigen::Affine3f& pose);
+    void viewQuantizedPointCloud(const pcl::PointCloud<PointT>::Ptr& cloud, const float& radius, const Eigen::Affine3f& pose);
 
-	/**
-   *Sets the virtual camera position in the virtual world
-   * @param 3 floats: x, y and z position
-   */
-	void setCameraPosition(const float& pos_x, const float& pos_y, const float& pos_z);
+    /**
+     * Updates all given edges in the PCLVisualizer.
+     * @param edges to be removed as a Vector of Edge(common_types.h)
+     */
+    void updateEdges(const std::vector<Edge>& edges);
 
-	/**
+    /**
+     * Updates all keyframes (ref. frames and point clouds).
+     * @param keyframes vector of keyframes
+     */
+    void updateKeyframes(const std::vector<Keyframe>& keyframes);
+
+    /**
+   	 *Sets the virtual camera position in the virtual world
+     * @param pos_x x camera position 
+     * @param pos_y y camera position 
+	 * @param pos_z z camera position 
+     */
+    void setCameraPosition(const float& pos_x, const float& pos_y, const float& pos_z);
+
+    /**
 	 * Wrapper to the interactor function of pcl::visualizer
 	 */
-	void spin();
+    void spin();
 
-	/**
+    /**
 	 * Wrapper to the interactor function of pcl::visualizer
 	 */
-	void spinOnce();
-
-	//Adds a line between the previous and the current camera position
-	void addCameraPath(const Eigen::Affine3f& pose);
+    void spinOnce();
 };
 
 #endif /* INCLUDE_RECONSTRUCTION_VISUALIZER_H_ */
