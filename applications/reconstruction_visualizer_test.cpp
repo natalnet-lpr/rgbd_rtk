002cdf8ed0d44eb8e97c119ce06b526bb4e7c047
Bruno Silva <brunomfs@gmail.com>
2020-09-14 17:20:28 -0300

    Changed number of required args.
    Also added cosmetic changes and the Logger level
    for the message that should appear when
    no arguments are provided (changed from L_INFO to L_ERROR).

--- applications/reconstruction_visualizer_test.cpp
+++ applications/reconstruction_visualizer_test.cpp
@@ -1,44 +1,40 @@
-/*
+/* 
  *  Software License Agreement (BSD License)
  *
- *  Copyright (c) 2016-2019, Natalnet Laboratory for Perceptual Robotics
+ *  Copyright (c) 2016-2020, Natalnet Laboratory for Perceptual Robotics
  *  All rights reserved.
- *  Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided
+ *  Redistribution and use in source and binary forms, with or without modification, are permitted provided
  *  that the following conditions are met:
  *
- *  1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and
+ *  1. Redistributions of source code must retain the above copyright notice, this list of conditions and
  *     the following disclaimer.
  *
- *  2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and
- *     the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- *
- *  3. Neither the name of the copyright holder nor the names of its
- * contributors may be used to endorse or
- *     promote products derived from this software without specific prior
- * written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, *  INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE
- *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR
- *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY,
- *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *     the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 
+ *  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
+ *     promote products derived from this software without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *  Author:
  *
+ *  Rodrigo Sarmento
  *  Bruno Silva
  */
+
+#include <Eigen/Geometry>
+#include <cstdio>
+#include <cstdlib>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <config_loader.h>
 #include <event_logger.h>
@@ -47,19 +43,12 @@
 #include <reconstruction_visualizer.h>
 #include <rgbd_loader.h>
 
-#include <Eigen/Geometry>
-#include <cstdio>
-#include <cstdlib>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
 using namespace std;
 using namespace cv;
 
 /**
- * This program shows the use of camera pose estimation using optical flow
- * visual odometry.
+ * This program shows how to use the ReconstructionVisualizer
+ * class.
  * @param .yml config. file (from which index_file is used)
  */
 int main(int argc, char **argv)
@@ -79,10 +68,8 @@ int main(int argc, char **argv)
 
     if (argc != 2)
     {
-        logger.print(EventLogger::L_INFO,
+        logger.print(EventLogger::L_ERROR,
                      "[reconstruction_visualizer_test.cpp] Usage: %s <path/to/config_file.yaml> ",
-                     argv[0]);
-        logger.print(EventLogger::L_INFO, "flag (1 to Edge, any other number to Keyframe)\n",
                      argv[0]);
         exit(0);
     }
@@ -117,23 +104,23 @@ int main(int argc, char **argv)
 
         visualizer.viewReferenceFrame(vo.pose_);
         visualizer.viewPointCloud(vo.curr_dense_cloud_, vo.pose_);
-        if (is_kf)
+        if(is_kf)
         {
             if (num_keyframes == 1)
                 kf_to = vo.getLastKeyframe(); // Save the first keyframe
             else
             {
-                // Update the last and the actual keyframe
+                // Update the last and the current keyframe
                 kf_from = kf_to;
                 kf_to = vo.getLastKeyframe();
                 kf_to.name_ = "key_frame_" + to_string(num_keyframes);
                 visualizer.addQuantizedPointCloud(kf_to.local_cloud_, 0.02, kf_to.pose_);
                 visualizer.addKeyFrame(kf_to, kf_to.name_); // Add Keyframe to the visualizer
 
-                // Create a vector 3d for the last and actual kf pose
+                // Create a vector 3d for the last and current kf pose
                 Eigen::Vector3d from(kf_from.pose_(0, 3), kf_from.pose_(1, 3), kf_from.pose_(2, 3));
                 Eigen::Vector3d to(kf_to.pose_(0, 3), kf_to.pose_(1, 3), kf_to.pose_(2, 3));
-                // Creates and add an Edge to the visualizer
+                // Create and add an Edge to the visualizer
                 Edge edge(num_keyframes - 1, num_keyframes, from, to, to_string(num_keyframes));
                 visualizer.addEdge(edge);
             }
