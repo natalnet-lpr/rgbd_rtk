e668dcd44ed2cd581bdaad58cb52c32ca655009a
Bruno Silva <brunomfs@gmail.com>
2020-09-14 17:45:33 -0300

    Cosmetic changes.

--- visualization/reconstruction_visualizer.h
+++ visualization/reconstruction_visualizer.h
@@ -1,7 +1,7 @@
 /* 
  *  Software License Agreement (BSD License)
  *
- *  Copyright (c) 2016, Natalnet Laboratory for Perceptual Robotics
+ *  Copyright (c) 2016-2020, Natalnet Laboratory for Perceptual Robotics
  *  All rights reserved.
  *  Redistribution and use in source and binary forms, with or without modification, are permitted provided
  *  that the following conditions are met:
@@ -22,6 +22,10 @@
  *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ *  Authors:
+ *
+ *  Bruno Silva
+ *  Rodrigo Sarmento Xavier
  */
 
 #ifndef INCLUDE_RECONSTRUCTION_VISUALIZER_H_
