0e402eb0196b1ff533b400ed6e4bc9d226cec00b
Bruno Silva <brunomfs@gmail.com>
2021-03-02 17:51:45 -0300

    Added method "checkAndGetBool" and changed formatting.

--- io/config_loader.cpp
+++ io/config_loader.cpp
@@ -1,7 +1,7 @@
 /* 
  *  Software License Agreement (BSD License)
  *
- *  Copyright (c) 2016-2020, Natalnet Laboratory for Perceptual Robotics
+ *  Copyright (c) 2016-2021, Natalnet Laboratory for Perceptual Robotics
  *  All rights reserved.
  *  Redistribution and use in source and binary forms, with or without modification, are permitted provided
  *  that the following conditions are met:
@@ -44,8 +44,8 @@ using namespace cv;
 void ConfigLoader::loadFile(const string& filename)
 {
 
-    fs.open(filename, FileStorage::READ);  //Reading config file
-    if(fs.isOpened() == false)
+    fs_.open(filename, FileStorage::READ);  //Reading config file
+    if(fs_.isOpened() == false)
     {
         logger.print(EventLogger::L_ERROR, "[config_loader.cpp] ERROR: ConfigFile couldn't be opened, check if your path is right\n");
         exit(0);
@@ -53,49 +53,73 @@ void ConfigLoader::loadFile(const string& filename)
 }
 
 bool ConfigLoader::checkAndGetInt(const string& parameter, int& parameter_int){
-    if(fs[parameter].empty()){
+    if(fs_[parameter].empty())
+    {
         logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s couldn't be loaded, check your ConfigFile.yaml\n", parameter.c_str());
         return false;
     } 
-    else{
-        fs[parameter] >> parameter_int;
+    else
+    {
+        fs_[parameter] >> parameter_int;
         logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s = %i\n", parameter.c_str(), parameter_int);
         return true;
     }
 }
 
 bool ConfigLoader::checkAndGetFloat(const string& parameter, float& parameter_float){
-    if(fs[parameter].empty()){
+    if(fs_[parameter].empty())
+    {
         logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s couldn't be loaded, check your ConfigFile.yaml\n", parameter.c_str());
         return false;
     }     
-    else{
-        fs[parameter] >> parameter_float;
+    else
+    {
+        fs_[parameter] >> parameter_float;
         logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s = %f\n", parameter.c_str(), parameter_float);
         return true;
     }
 }
 
 bool ConfigLoader::checkAndGetString(const string& parameter, string& parameter_string){
-    if(fs[parameter].empty()){
+    if(fs_[parameter].empty())
+    {
         logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s couldn't be loaded, check your ConfigFile.yaml\n", parameter.c_str());
         return false;
     }     
-    else{
-        fs[parameter] >> parameter_string;
+    else
+    {
+        fs_[parameter] >> parameter_string;
         logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s = %s\n", parameter.c_str(), parameter_string.c_str());
+        return true;
+    }
+}
+
+bool ConfigLoader::checkAndGetBool(const string& parameter, bool& parameter_bool){
+    if(fs_[parameter].empty())
+    {
+        logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s couldn't be loaded, check your ConfigFile.yaml\n", parameter.c_str());
+        return false;
+    }     
+    else
+    {
+        string parameter_string;
+        fs_[parameter] >> parameter_string;
+        parameter_bool = parameter_string == "true" ? 1 : 0;
+        logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s = %i\n", parameter.c_str(), parameter_bool);
         return true;
     }
 }
 
 bool ConfigLoader::checkAndGetMat(const string& parameter, Mat& parameter_mat)
 {
-    if(fs[parameter].empty()){
+    if(fs_[parameter].empty())
+    {
         logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s couldn't be loaded, check your ConfigFile.yaml\n", parameter.c_str());
         return false;
     }     
-    else{
-        fs[parameter] >> parameter_mat;
+    else
+    {
+        fs_[parameter] >> parameter_mat;
         logger.print(EventLogger::L_INFO, "[config_loader.cpp] INFO: Parameter %s loaded.\n", parameter.c_str());
         return true;
     }
--- io/config_loader.h
+++ io/config_loader.h
@@ -1,36 +1,25 @@
-/*
+/* 
  *  Software License Agreement (BSD License)
  *
- *  Copyright (c) 2016-2019, Natalnet Laboratory for Perceptual Robotics
+ *  Copyright (c) 2016-2021, Natalnet Laboratory for Perceptual Robotics
  *  All rights reserved.
- *  Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided
+ *  Redistribution and use in source and binary forms, with or without modification, are permitted provided
  *  that the following conditions are met:
  *
- *  1. Redistributions of source code must retain the above copyright notice, this list of
- * conditions and
+ *  1. Redistributions of source code must retain the above copyright notice, this list of conditions and
  *     the following disclaimer.
  *
- *  2. Redistributions in binary form must reproduce the above copyright notice, this list of
- * conditions and
- *     the following disclaimer in the documentation and/or other materials provided with the
- * distribution.
- *
- *  3. Neither the name of the copyright holder nor the names of its contributors may be used to
- * endorse or
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *     the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 
+ *  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
  *     promote products derived from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL,
- *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR
- *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY,
- *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *  Authors:
@@ -55,12 +44,13 @@ using namespace std;
 class ConfigLoader
 {
 public:
-    cv::FileStorage fs;
+
     /**
      * Load configuration file.
      * Uses the default path "../config_files/tum_odometry.yaml"
      */
     ConfigLoader();
+
     /**
      * Load configuration file.
      * @param filename: path where the config. file is located
@@ -76,6 +66,7 @@ public:
     * @return boolean, false if the parameter is not in configfile and true if it is
     */
     bool checkAndGetInt(const string &parameter, int &parameter_int);
+
     /**
      * Get a float parameter in ConfigFile
      * @param parameter with the name of the parameter in configfile
@@ -83,6 +74,7 @@ public:
      * @return boolean, false if the parameter is not in configfile and true if it is
      */
     bool checkAndGetFloat(const string &parameter, float &parameter_float);
+
     /**
      * Get a string parameter in ConfigFile
      * @param parameter with the name of the parameter in configfile
@@ -90,13 +82,27 @@ public:
      * @return boolean, false if the parameter is not in configfile and true if it is
      */
     bool checkAndGetString(const string &parameter, string &parameter_string);
+
     /**
-     * Get a string parameter in ConfigFile
+     * Get a bool parameter in ConfigFile
      * @param parameter with the name of the parameter in configfile
-     * @param parameter_mat string where the value will be returned
+     * @param parameter_bool string where the value will be returned
+     * @return boolean, false if the parameter is not in configfile and true if it is
+     */
+    bool checkAndGetBool(const string &parameter, bool &parameter_bool);
+
+    /**
+     * Get a cv::Mat parameter in ConfigFile
+     * @param parameter with the name of the parameter in configfile
+     * @param parameter_mat cv::Mat where the value will be returned
      * @return boolean, false if the parameter is not in configfile and true if it is
      */
     bool checkAndGetMat(const string &parameter, cv::Mat &parameter_mat);
+
+private:
+
+    cv::FileStorage fs_;
+
 };
 
 #endif
