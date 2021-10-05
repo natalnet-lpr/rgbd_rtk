e6850962d0295a59f0b9d6022e494f7e664e63a8
luiz felipe <y9luiz@hotmail.com>
2021-10-05 11:16:42 -0300

    feature map now compiling with opencv4

--- tracking/feature_map_tracker.cpp
+++ tracking/feature_map_tracker.cpp
@@ -292,7 +292,13 @@ void FeatureMapTracker::setFeatureDetector(const std::string& feature_detector)
     else if (upper_feature_detector == "SURF")
         feature_detector_ = cv::xfeatures2d::SURF::create();
     else if (upper_feature_detector == "SIFT")
-        feature_detector_ = cv::xfeatures2d::SIFT::create();
+    {
+        #if CV_MAJOR_VERSION < 4
+            feature_detector_ = cv::xfeatures2d::SIFT::create();
+        #else
+            feature_detector_ = cv::SIFT::create();  
+        #endif
+    }
     else
     {
         MLOG_ERROR(
@@ -325,7 +331,13 @@ void FeatureMapTracker::setDescriptorExtractor(const std::string& descriptor_ext
     else if (upper_descriptor_extractor == "SURF")
         descriptor_extractor_ = cv::xfeatures2d::SURF::create();
     else if (upper_descriptor_extractor == "SIFT")
-        descriptor_extractor_ = cv::xfeatures2d::SIFT::create();
+    {
+        #if CV_MAJOR_VERSION < 4
+            descriptor_extractor_ = cv::xfeatures2d::SIFT::create();
+        #else
+            descriptor_extractor_ = cv::SIFT::create();  
+        #endif
+    }
     else
     {
         MLOG_ERROR(
@@ -440,7 +452,11 @@ bool FeatureMapTracker::track(const cv::Mat& img)
     if (img.channels() > 1)
     {
         MLOG_DEBUG(EventLogger::M_TRACKING, "@FeatureMapTracker::track: converting to a grayscale image...\n");
-        cvtColor(img, curr_frame_gray_, CV_BGR2GRAY);
+        #if CV_MAJOR_VERSION < 4
+            cvtColor(img, curr_frame_gray_, CV_BGR2GRAY);
+        #else
+            cvtColor(img, curr_frame_gray_, COLOR_BGR2GRAY);
+        #endif
     }
     else
     {
@@ -469,10 +485,15 @@ bool FeatureMapTracker::track(const cv::Mat& img)
         MLOG_DEBUG(EventLogger::M_TRACKING, "@FeatureMapTracker::track: inicial map size: %i\n", map_descriptors_.rows);
 
         MLOG_DEBUG(EventLogger::M_TRACKING, "@FeatureMapTracker::track: building the Index object...\n");
-
-        double el_time = (double)cvGetTickCount();
-        index_->build(map_descriptors_, cv::flann::KDTreeIndexParams(), cvflann::FLANN_DIST_EUCLIDEAN);
-        el_time = ((double)cvGetTickCount() - el_time) / (cvGetTickFrequency() * 1000.0);
+        #if CV_MAJOR_VERSION < 4
+            double el_time = (double)cvGetTickCount();
+            index_->build(map_descriptors_, cv::flann::KDTreeIndexParams(), cvflann::FLANN_DIST_EUCLIDEAN);
+            el_time = ((double)cvGetTickCount() - el_time) / (cvGetTickFrequency() * 1000.0);
+        #else
+            double el_time = (double)getTickCount();
+            index_->build(map_descriptors_, cv::flann::KDTreeIndexParams(), cvflann::FLANN_DIST_EUCLIDEAN);
+            el_time = ((double)getTickCount() - el_time) / (getTickFrequency() * 1000.0);
+        #endif
 
         MLOG_DEBUG(EventLogger::M_TRACKING, "@FeatureMapTracker::track: build index time: %f ms\n", el_time);
 
