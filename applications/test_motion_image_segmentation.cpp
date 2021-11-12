26fa0575452f2301608de678065bd18429eb0f6f
luiz felipe <y9luiz@hotmail.com>
2021-11-11 22:01:07 -0300

    added video support

--- applications/test_motion_image_segmentation.cpp
+++ applications/test_motion_image_segmentation.cpp
@@ -4,9 +4,16 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <config_loader.h>
+#include <rgbd_loader.h>
+
 using namespace std;
 typedef std::unordered_map<int,std::string> classMap;
-
+void mergeMask(cv::Mat & img, cv::Mat mask)
+{
+    img.convertTo(img,CV_8UC1);
+    cv::cvtColor(img,img,cv::COLOR_BGR2GRAY);
+    bitwise_and(img,mask,img);
+}
 int main(int argc, char ** argv)
 {       
 
@@ -16,35 +23,88 @@ int main(int argc, char ** argv)
         return 1;
     }
 
-    // load parameters
+    // load parameters    
     ConfigLoader param_loader(argv[1]);
     std::string model_path;  
     param_loader.checkAndGetString("dnn_model_path",model_path);
     std::string config_file; 
     param_loader.checkAndGetString("dnn_config_file_path",config_file);
-    std::string img_path;
-    param_loader.checkAndGetString("image_path",img_path);
+    bool is_video;
+    param_loader.checkAndGetBool("is_video",is_video);
+    bool resize_src;
+    param_loader.checkAndGetBool("resize_src",resize_src);
+    string src_path;
+    param_loader.checkAndGetString("src_path",src_path);
+    
 
     // create image segmentor
     auto segmenter = FactoryMotionSegmenter::create(MotionSegmenterType::DNN,model_path,
                             config_file,classMap({{0,"person"},{2,"car"}}));
+
+    if(is_video)
+    {           
+        cv::VideoCapture cap(src_path);
+
+        if(!cap.isOpened()){
+            cerr << "invalid video file "<< src_path << endl;
+            exit(-1);
+        }
+        cv::Mat frame;
+        while(cap.isOpened())
+        {
+            cv::Mat mask;
+            cap >> frame;
+            if(resize_src)
+            {
+                cv::resize(frame,frame,cv::Size(640,480));
+            }
+            // segment the image
+            segmenter->segment(frame, mask);
+
+            cv::imshow("original img", frame);
+            cv::imshow("mask", mask);
+
+            mergeMask(frame,mask);
+
+            cv::imshow("masked img", frame);
+
+            char k = cv::waitKey(1);
+            if ( k == 27)
+            {
+                break;
+            }
+        }
+        return 0;
+        
+    }
+    else{
+
+        param_loader.checkAndGetString("src_path",src_path);
+    
+        cv::Mat in_img = cv::imread(src_path);
+        if(resize_src)
+        {
+            cv::resize(in_img,in_img,cv::Size(300,300));
+        }
+        if(in_img.empty()){
+            cerr << "invalid img file "<< src_path << endl;
+            exit(-1);
+        }
+        cv::Mat mask;
+        // segment the image
+        segmenter->segment(in_img, mask);
+
+        cv::imshow("original img", in_img);
+        cv::imshow("mask", mask);
+
+        mergeMask(in_img,mask);
+
+        cv::imshow("masked img", in_img);
+
+        cv::waitKey(10000);
+    
+    }
    
-    cv::Mat in_img = cv::imread(img_path);
-    cv::Mat out_mask;
-    
-    // segment the image
-    segmenter->segment(in_img, out_mask);
-
-    cv::imshow("original img", in_img);
-    cv::imshow("mask", out_mask);
-
-    in_img.convertTo(in_img,CV_8UC1);
-    cv::cvtColor(in_img,in_img,cv::COLOR_BGR2GRAY);
-    
-    bitwise_and(in_img,out_mask,in_img);
-    cv::imshow("masked img", in_img);
-
-    cv::waitKey(10000);
 
     return 0;
 }
