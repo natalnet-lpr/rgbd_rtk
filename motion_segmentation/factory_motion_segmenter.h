8524f0c5bc011694cbe4ace79d65014ee9a6eb4e
Luiz Felipe M. Correia (CW) <lcorreia@teradici.com>
2022-01-20 11:40:07 -0300

    added static assert if not create object

--- motion_segmentation/factory_motion_segmenter.h
+++ motion_segmentation/factory_motion_segmenter.h
@@ -7,27 +7,30 @@
 #include <stdarg.h>
 #include <memory>
 
-enum class MotionSegmenterType{
+enum class MotionSegmenterType : uint8_t
+{
     DNN,
     Geometric
 };
 
-class FactoryMotionSegmenter{
-    public:
-        // possible to construct
-        // see https://en.cppreference.com/w/cpp/language/sfinae
-        template <typename T, typename... Args>
-        static std::enable_if_t<std::is_constructible<T, Args...>::value, std::shared_ptr<T>>
-        create(Args&&... args)
-        {
-            return std::make_shared<T>(std::forward<Args>(args)...);
-        }
-        // possible to construct
-        template <typename T, typename... Args>
-        static std::enable_if_t< !std::is_constructible<T, Args...>::value, std::shared_ptr<T>>
-        create(Args&&... args)
-        {
-            return nullptr;
-        }
-        
+class FactoryMotionSegmenter
+{
+public:
+    // possible to construct
+    // see https://en.cppreference.com/w/cpp/language/sfinae
+    template <typename T, typename... Args>
+    static std::enable_if_t<std::is_constructible<T, Args...>::value, std::shared_ptr<T>>
+    create(Args &&...args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+    // possible to construct
+    template <typename T, typename... Args>
+    static std::enable_if_t<!std::is_constructible<T, Args...>::value, std::shared_ptr<T>>
+    create(Args &&...args)
+    {
+        // Bruno must revise this
+        static_assert(std::is_constructible<T, Args...>::value, "error to create Motion Segmenter"); // you could use tis approach to stop compile if could not create
+        return nullptr;
+    }
 };
